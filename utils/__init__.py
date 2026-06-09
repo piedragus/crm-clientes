8e30fba5f751c84af279a57f0f0f4d7b762f13d2
Claudio <claudio@crm-clientes>
2026-06-09 10:22:01 +0000

    feat: agregar schema y metodos db de aliases

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,7 @@
+# Re-export everything from the original utils.py (now utils_legacy.py)
+from utils.utils_legacy import Config, BackupManager, Exportador, CSVCleaner, formatear_fecha
+
+# Aliases feature additions
 from utils.normalizacion import normalizar_alias_empresa
 from utils.excepciones import (
     AliasValidationError,
